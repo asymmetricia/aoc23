eb1a714ce06469759f9046a631319915fefdc0fd
Tricia Bogen <tricia-github@cernu.us>
2023-12-15 18:50:20 -0800

    day 7 doneeee

--- day07/a/main.go
+++ day07/a/main.go
@@ -3,32 +3,119 @@ package main
 import (
 	"bytes"
 	"os"
+	"sort"
 	"strings"
 	"unicode"
 
 	"github.com/sirupsen/logrus"
+	"golang.org/x/exp/slices"
 
 	"github.com/asymmetricia/aoc23/aoc"
 )
 
 var log = logrus.StandardLogger()
 
+type Type int
+
+const (
+	HighCard Type = iota
+	OnePair
+	TwoPair
+	ThreeOfAKind
+	FullHouse
+	FourOfAKind
+	FiveOfAKind
+)
+
+func HandType(a string) Type {
+	h := []rune(a)
+	sort.Slice(h, func(i, j int) bool {
+		return h[i] < h[j]
+	})
+	if h[0] == h[1] && h[1] == h[2] && h[2] == h[3] && h[3] == h[4] {
+		return FiveOfAKind
+	}
+
+	if h[0] == h[1] && h[1] == h[2] && h[2] == h[3] ||
+		h[1] == h[2] && h[2] == h[3] && h[3] == h[4] {
+		return FourOfAKind
+	}
+
+	if h[0] == h[1] && h[2] == h[3] && h[3] == h[4] ||
+		h[0] == h[1] && h[1] == h[2] && h[3] == h[4] {
+		return FullHouse
+	}
+
+	if h[0] == h[1] && h[1] == h[2] ||
+		h[1] == h[2] && h[2] == h[3] ||
+		h[2] == h[3] && h[3] == h[4] {
+		return ThreeOfAKind
+	}
+	if h[0] == h[1] && h[2] == h[3] ||
+		h[0] == h[1] && h[3] == h[4] ||
+		h[1] == h[2] && h[3] == h[4] {
+		return TwoPair
+	}
+	if h[0] == h[1] || h[1] == h[2] || h[2] == h[3] || h[3] == h[4] {
+		return OnePair
+	}
+
+	return HighCard
+}
+
+var values = map[uint8]int{
+	'2': 2,
+	'3': 3,
+	'4': 4,
+	'5': 5,
+	'6': 6,
+	'7': 7,
+	'8': 8,
+	'9': 9,
+	'T': 10,
+	'J': 11,
+	'Q': 12,
+	'K': 13,
+	'A': 14}
+
+func LessHand(a, b Hand) bool {
+	ha, hb := HandType(a.Hand), HandType(b.Hand)
+	if ha != hb {
+		return ha < hb
+	}
+
+	for i := 0; i < 5; i++ {
+		if a.Hand[i] != b.Hand[i] {
+			return values[a.Hand[i]] < values[b.Hand[i]]
+		}
+	}
+
+	return false
+}
+
+type Hand struct {
+	Hand string
+	Bid  int
+}
+
 func solution(name string, input []byte) int {
 	// trim trailing space only
 	input = bytes.Replace(input, []byte("\r"), []byte(""), -1)
 	input = bytes.TrimRightFunc(input, unicode.IsSpace)
 	lines := strings.Split(strings.TrimRightFunc(string(input), unicode.IsSpace), "\n")
-	uniq := map[string]bool{}
+	var hands []Hand
 	for _, line := range lines {
-		uniq[line] = true
+		hb := strings.Fields(line)
+		hands = append(hands, Hand{hb[0], aoc.MustAtoi(hb[1])})
 	}
-	log.Printf("read %d %s lines (%d unique)", len(lines), name, len(uniq))
 
-	//for _, line := range lines {
-	//	//fields := strings.Fields(line)
-	//}
+	slices.SortFunc(hands, LessHand)
 
-	return -1
+	var accum int = 0
+	for i, h := range hands {
+		accum += (i + 1) * h.Bid
+	}
+	return accum
 }
 
 func main() {
--- day07/b/main.go
+++ day07/b/main.go
@@ -3,32 +3,136 @@ package main
 import (
 	"bytes"
 	"os"
+	"sort"
 	"strings"
 	"unicode"
 
 	"github.com/sirupsen/logrus"
+	"golang.org/x/exp/slices"
 
 	"github.com/asymmetricia/aoc23/aoc"
 )
 
 var log = logrus.StandardLogger()
 
+type Type int
+
+const (
+	HighCard Type = iota
+	OnePair
+	TwoPair
+	ThreeOfAKind
+	FullHouse
+	FourOfAKind
+	FiveOfAKind
+)
+
+func HandType(a string) Type {
+	hand := []rune(a)
+	for i, card := range hand {
+		if card == 'J' {
+			newHand := slices.Clone(hand)
+			best := HighCard
+			for k := range values {
+				if k == 'J' {
+					continue
+				}
+				newHand[i] = rune(k)
+				newHandType := HandType(string(newHand))
+				if newHandType > best {
+					best = newHandType
+				}
+			}
+			return best
+		}
+	}
+	sort.Slice(hand, func(i, j int) bool {
+		return hand[i] < hand[j]
+	})
+	if hand[0] == hand[1] && hand[1] == hand[2] && hand[2] == hand[3] && hand[3] == hand[4] {
+		return FiveOfAKind
+	}
+
+	if hand[0] == hand[1] && hand[1] == hand[2] && hand[2] == hand[3] ||
+		hand[1] == hand[2] && hand[2] == hand[3] && hand[3] == hand[4] {
+		return FourOfAKind
+	}
+
+	if hand[0] == hand[1] && hand[2] == hand[3] && hand[3] == hand[4] ||
+		hand[0] == hand[1] && hand[1] == hand[2] && hand[3] == hand[4] {
+		return FullHouse
+	}
+
+	if hand[0] == hand[1] && hand[1] == hand[2] ||
+		hand[1] == hand[2] && hand[2] == hand[3] ||
+		hand[2] == hand[3] && hand[3] == hand[4] {
+		return ThreeOfAKind
+	}
+	if hand[0] == hand[1] && hand[2] == hand[3] ||
+		hand[0] == hand[1] && hand[3] == hand[4] ||
+		hand[1] == hand[2] && hand[3] == hand[4] {
+		return TwoPair
+	}
+	if hand[0] == hand[1] || hand[1] == hand[2] || hand[2] == hand[3] || hand[3] == hand[4] {
+		return OnePair
+	}
+
+	return HighCard
+}
+
+var values = map[uint8]int{
+	'2': 2,
+	'3': 3,
+	'4': 4,
+	'5': 5,
+	'6': 6,
+	'7': 7,
+	'8': 8,
+	'9': 9,
+	'T': 10,
+	'J': 1,
+	'Q': 12,
+	'K': 13,
+	'A': 14}
+
+func LessHand(a, b Hand) bool {
+	ha, hb := HandType(a.Hand), HandType(b.Hand)
+	if ha != hb {
+		return ha < hb
+	}
+
+	for i := 0; i < 5; i++ {
+		if a.Hand[i] != b.Hand[i] {
+			return values[a.Hand[i]] < values[b.Hand[i]]
+		}
+	}
+
+	return false
+}
+
+type Hand struct {
+	Hand string
+	Bid  int
+}
+
 func solution(name string, input []byte) int {
 	// trim trailing space only
 	input = bytes.Replace(input, []byte("\r"), []byte(""), -1)
 	input = bytes.TrimRightFunc(input, unicode.IsSpace)
 	lines := strings.Split(strings.TrimRightFunc(string(input), unicode.IsSpace), "\n")
-	uniq := map[string]bool{}
+	var hands []Hand
 	for _, line := range lines {
-		uniq[line] = true
+		hb := strings.Fields(line)
+		hands = append(hands, Hand{hb[0], aoc.MustAtoi(hb[1])})
 	}
-	log.Printf("read %d %s lines (%d unique)", len(lines), name, len(uniq))
 
-	//for _, line := range lines {
-	//	//fields := strings.Fields(line)
-	//}
+	slices.SortFunc(hands, LessHand)
 
-	return -1
+	var accum int = 0
+	for i, h := range hands {
+		accum += (i + 1) * h.Bid
+	}
+	return accum
 }
 
 func main() {
