59dacb1546ef16da3c71954ccd77b871b7d41d4e
Tricia Bogen <tricia-github@cernu.us>
2024-11-01 19:01:45 -0700

    day 11

--- day11/a/main.go
+++ day11/a/main.go
@@ -2,16 +2,64 @@ package main
 
 import (
 	"bytes"
+	"github.com/asymmetricia/aoc23/aoc"
+	"github.com/asymmetricia/aoc23/coord"
+	"golang.org/x/exp/slices"
 	"os"
 	"strings"
 	"unicode"
 
 	"github.com/sirupsen/logrus"
-
-	"github.com/asymmetricia/aoc23/aoc"
 )
 
 var log = logrus.StandardLogger()
+
+func expand(world coord.World) {
+	minx, miny, maxx, maxy := world.Rect()
+cols:
+	for x := minx; x <= maxx; x++ {
+		for y := miny; y <= maxy; y++ {
+			if world.At(coord.C(x, y)) == '#' {
+				continue cols
+			}
+		}
+		galaxies := world.Find('#')
+		slices.SortFunc(galaxies, func(a, b coord.Coord) bool {
+			return b.X < a.X
+		})
+		for _, c := range galaxies {
+			if c.X <= x {
+				continue
+			}
+			world.Set(c.East(), '#')
+			world.Set(c, '>')
+		}
+		maxx++
+		x++
+	}
+
+rows:
+	for y := miny; y <= maxy; y++ {
+		for x := minx; x <= maxx; x++ {
+			if world.At(coord.C(x, y)) == '#' {
+				continue rows
+			}
+		}
+		galaxies := world.Find('#')
+		slices.SortFunc(galaxies, func(a, b coord.Coord) bool {
+			return b.Y < a.Y
+		})
+		for _, c := range galaxies {
+			if c.Y <= y {
+				continue
+			}
+			world.Set(c.South(), '#')
+			world.Set(c, 'v')
+		}
+		maxy++
+		y++
+	}
+}
 
 func solution(name string, input []byte) int {
 	// trim trailing space only
@@ -24,11 +72,24 @@ func solution(name string, input []byte) int {
 	}
 	log.Printf("read %d %s lines (%d unique)", len(lines), name, len(uniq))
 
-	//for _, line := range lines {
-	//	//fields := strings.Fields(line)
-	//}
+	world := coord.Load(lines, coord.LoadConfig{Ignore: "."})
 
-	return -1
+	world.Print()
+	expand(world)
+	world.Print()
+
+	var sum int
+	galaxies := world.Find('#')
+	for i := 0; i < len(galaxies); i++ {
+		iC := galaxies[i]
+		for j := i + 1; j < len(galaxies); j++ {
+			jC := galaxies[j]
+			//log.Printf("%s -> %s == %d", iC, jC, iC.TaxiDistance(jC))
+			sum += iC.TaxiDistance(jC)
+		}
+	}
+
+	return sum
 }
 
 func main() {
--- day11/b/main.go
+++ day11/b/main.go
@@ -2,18 +2,69 @@ package main
 
 import (
 	"bytes"
+	"github.com/asymmetricia/aoc23/aoc"
+	"github.com/asymmetricia/aoc23/coord"
+	"golang.org/x/exp/slices"
+	"math/big"
 	"os"
 	"strings"
 	"unicode"
 
 	"github.com/sirupsen/logrus"
-
-	"github.com/asymmetricia/aoc23/aoc"
 )
 
 var log = logrus.StandardLogger()
 
-func solution(name string, input []byte) int {
+func expand(world coord.World) {
+	const amount = 1e6 - 1
+
+	minx, miny, maxx, maxy := world.Rect()
+cols:
+	for x := minx; x <= maxx; x++ {
+		for y := miny; y <= maxy; y++ {
+			if world.At(coord.C(x, y)) == '#' {
+				continue cols
+			}
+		}
+		galaxies := world.Find('#')
+		slices.SortFunc(galaxies, func(a, b coord.Coord) bool {
+			return b.X < a.X
+		})
+		for _, c := range galaxies {
+			if c.X <= x {
+				continue
+			}
+			world.Set(coord.C(c.X+amount, c.Y), '#')
+			world.Set(c, 0)
+		}
+		maxx += amount
+		x += amount
+	}
+
+rows:
+	for y := miny; y <= maxy; y++ {
+		for x := minx; x <= maxx; x++ {
+			if world.At(coord.C(x, y)) == '#' {
+				continue rows
+			}
+		}
+		galaxies := world.Find('#')
+		slices.SortFunc(galaxies, func(a, b coord.Coord) bool {
+			return b.Y < a.Y
+		})
+		for _, c := range galaxies {
+			if c.Y <= y {
+				continue
+			}
+			world.Set(coord.C(c.X, c.Y+amount), '#')
+			world.Set(c, 0)
+		}
+		maxy += amount
+		y += amount
+	}
+}
+
+func solution(name string, input []byte) *big.Int {
 	// trim trailing space only
 	input = bytes.Replace(input, []byte("\r"), []byte(""), -1)
 	input = bytes.TrimRightFunc(input, unicode.IsSpace)
@@ -24,11 +75,23 @@ func solution(name string, input []byte) int {
 	}
 	log.Printf("read %d %s lines (%d unique)", len(lines), name, len(uniq))
 
-	//for _, line := range lines {
-	//	//fields := strings.Fields(line)
-	//}
+	world := coord.Load(lines, coord.LoadConfig{Ignore: "."})
 
-	return -1
+	//world.Print()
+	expand(world)
+	//world.Print()
+
+	sum := &big.Int{}
+	galaxies := world.Find('#')
+	for i := 0; i < len(galaxies); i++ {
+		iC := galaxies[i]
+		for j := i + 1; j < len(galaxies); j++ {
+			jC := galaxies[j]
+			sum.Add(sum, big.NewInt(int64(iC.TaxiDistance(jC))))
+		}
+	}
+
+	return sum
 }
 
 func main() {
