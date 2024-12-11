46e3fb67e7e5d18199cf6b946246fad9a1d959bb
Tricia Bogen <tricia-github@cernu.us>
2024-12-10 20:58:58 -0800

    day 11 init

---
+++ 2024/day11/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bytes"
+	"strings"
+	"unicode"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/asymmetricia/aoc23/aoc"
+)
+
+var log = logrus.StandardLogger()
+
+func solutionA(input []byte) int {
+	// trim trailing space only
+	input = bytes.Replace(input, []byte("\r"), []byte(""), -1)
+	input = bytes.TrimRightFunc(input, unicode.IsSpace)
+	lines := strings.Split(strings.TrimRightFunc(string(input), unicode.IsSpace), "\n")
+	uniq := map[string]bool{}
+	for _, line := range lines {
+		uniq[line] = true
+	}
+	log.Printf("read %d lines (%d unique)", len(lines), len(uniq))
+
+	//for _, line := range lines {
+	//	//fields := strings.Fields(line)
+	//}
+
+	return -1
+}
+
+func solutionB(input []byte) int {
+	// trim trailing space only
+	input = bytes.Replace(input, []byte("\r"), []byte(""), -1)
+	input = bytes.TrimRightFunc(input, unicode.IsSpace)
+
+	// lines := strings.Split(strings.TrimRightFunc(string(input), unicode.IsSpace), "\n")
+	//for _, line := range lines {
+	//	//fields := strings.Fields(line)
+	//}
+
+	return -1
+}
+
+func main() {
+	log.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp:   true,
+		TimestampFormat: "2006-01-02T15:04:05",
+	})
+
+	input := aoc.Input(2024, 11)
+	log.Printf("input solution A: %d", solutionA(input))
+	log.Printf("input solution B: %d", solutionB(input))
+}
